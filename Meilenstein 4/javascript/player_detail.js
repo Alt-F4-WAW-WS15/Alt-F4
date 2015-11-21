a1a4bc4fd6d7a55c3db4e7d76a5e1f4ea4c69799
Endzeit89 <mad-x@hotmail.de>
2015-11-21 03:04:51 +0100

    Added: Meilenstein 4, data.json , task 4 - Marcel

--- Meilenstein 4/javascript/player_detail.js
+++ Meilenstein 4/javascript/player_detail.js
@@ -6,4 +6,9 @@ function myFunction(a){
 		document.getElementById("my_favorites").setAttribute("style", "background-color: rgb(0,28,255); color: #FFFFFF;");
 		document.getElementById("all_players").setAttribute("style", "background-color: rgb(153,217,234); color: black;");
 	}
+	
+	
+	
+	
 }
+
