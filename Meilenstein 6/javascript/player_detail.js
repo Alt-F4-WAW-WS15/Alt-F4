fb374ff570ea5e2177f31a667451b8b69b64e22c
Testorlein <jonathan.brenner@gmx.de>
2016-01-09 23:54:19 +0100

    Added: Meilenstein 6, Node.js , Task1 - Jonathan

---
+++ Meilenstein 6/javascript/player_detail.js
@@ -0,0 +1,54 @@
+var url;
+
+function myFunction(a){
+	if(a == 1){
+		document.getElementById("all_players").setAttribute("style", "background-color: rgb(0,28,255); color: #FFFFFF;");
+		document.getElementById("my_favorites").setAttribute("style", "background-color: rgb(153,217,234); color: black;");
+		url = 'http://localhost:3000/AllPlayers';
+		sendRequest(true);
+	}else{
+		document.getElementById("my_favorites").setAttribute("style", "background-color: rgb(0,28,255); color: #FFFFFF;");
+		document.getElementById("all_players").setAttribute("style", "background-color: rgb(153,217,234); color: black;");
+		url = 'http://localhost:3000/Favorites';
+		sendRequest(false);
+	}
+}
+
+function sendRequest(fav) {
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status== 200) {
+			var json = JSON.parse(xmlhttp.responseText);
+			console.log(xmlhttp.responseText);
+			load(json);
+		}
+	}
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+function load(json) {
+	// erstelle den tabellen inhalt html anhand der json object datei
+	console.log('bin da und so');
+	var table = document.getElementById("table");
+	// löschen der alten nabs
+	while(table.rows.length > 2) {
+		table.deleteRow(2);
+	}
+	// erstellen der neuen nabs
+	for(var i = 0; i < json.length; i++) {
+			var row = table.insertRow(2);
+			var html = "<td class=entry>"+json[i].firstname+" "+json[i].surname+"</td>";
+			html += "<td class=entry>"+json[i].team+"</td>";
+			html += "<td class=entry>"+json[i].headcoach+"</td>";
+			html += "<td class=entry>"+json[i].asisstantcoach+"</td>";
+			html += "<td class=entry>"+json[i].position+"</td>";
+			if (json[i].isActive)
+				html += "<td class=entry>Ja</td>";
+			else
+				html += "<td class=entry>Nein</td>"
+			html += "<td class=entry>"+json[i].number+"</td>";
+			html += "<td class=entry>"+json[i].year+"</td>";
+			row.innerHTML = html;
+	}
+}
